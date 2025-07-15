0441cd0d8720f8aad0fa6e812c5b051a40afcd74
Anton <anton.tumurov2012@outlook.com>
2025-07-15 14:50:19 +1000

    Updated mobile page + support for Server deactivation

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ if (window.innerWidth <= 600 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMob
 const BIN_ID = "67b6c25cacd3cb34a8e9372c"; 
 const API_KEY = "$2a$10$coWigkYNJbj.z.EUyfjUgurk/Xkg/olZFEXFWplgUtfIhtY9m/rOC"; // (OK if public info only)
 
+function fallback(){
+    console.error("Server Down. Redirecting to fallback.");
+    location.href = "fallback.html"
+};
+
 fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
     headers: {
         "X-Access-Key": API_KEY
@@ -14,25 +19,33 @@ fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
 .then(res => res.json())
 .then(data => {
     const content = data.record;
-    const version = content.version || "[An error occured while fetching the version]";
-    const updaterlink = content.updater || "fallback.html";
-    const torrentlink = content.torrent || "fallback.html";
+    const status = content.status || false;
+    const version = content.version;
+    const updaterlink = content.updater;
+    const torrentlink = content.torrent;
 
-    // Replace torrent link
-    document.querySelectorAll(".torrent").forEach(torrentel => {
-        torrentel.href = torrentlink;
-    });
+    if (status !== true) {
+        fallback();
+    }
 
-    // Replace in download button
-    document.querySelectorAll(".download-btn").forEach(linkel => {
-        linkel.href = updaterlink;
-    });
+    if (window.location.pathname.endsWith("index.html") || window.location.pathname === "/" ) {
+        // Replace torrent link
+        document.querySelectorAll(".torrent").forEach(torrentel => {
+            torrentel.href = torrentlink;
+        });
 
-    // Replace in download button text
-    document.querySelectorAll(".download-btn-text").forEach(textel => {
-        textel.textContent = `Download Crypt ${version}`;
-    })
+        // Replace in download button
+        document.querySelectorAll(".download-btn").forEach(linkel => {
+            linkel.href = updaterlink;
+        });
+
+        // Replace in download button text
+        document.querySelectorAll(".download-btn-text").forEach(textel => {
+            textel.textContent = `Download Crypt ${version}`;
+        });
+    }
 })
 .catch(err => {
     console.error("Failed to load JSONBin data:", err);
+    fallback();
 });
