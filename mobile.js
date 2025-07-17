55967ddf05dbc665c254e334c9e48aca31adb1a0
Anton <anton.tumurov2012@outlook.com>
2025-07-18 08:35:49 +1000

    Seperate js files

--- mobile.js
+++ mobile.js
@@ -1,8 +1,3 @@
-// Redirect to mobile page if on mobile device
-if (window.innerWidth <= 600 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && !window.location.pathname.endsWith("mobile.html")) {
-    window.location.href = "mobile.html";
-}
-
 const BIN_ID = "67b6c25cacd3cb34a8e9372c"; 
 const API_KEY = "$2a$10$coWigkYNJbj.z.EUyfjUgurk/Xkg/olZFEXFWplgUtfIhtY9m/rOC"; // (OK if public info only)
 
@@ -27,21 +22,6 @@ fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
     if (status !== true) {
         fallback();
     }
-
-    // Replace torrent link
-    document.querySelectorAll(".torrent").forEach(torrentel => {
-        torrentel.href = torrentlink;
-    });
-
-    // Replace in download button
-    document.querySelectorAll(".download-btn").forEach(linkel => {
-        linkel.href = updaterlink;
-    });
-
-    // Replace in download button text
-    document.querySelectorAll(".download-btn-text").forEach(textel => {
-        textel.textContent = `Download Crypt ${version}`;
-    });
 })
 .catch(err => {
     console.error("Failed to load JSONBin data:", err);
